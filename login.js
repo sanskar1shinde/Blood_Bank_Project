7a89771132f57b76fa4204d386c2a9bb4f21130b
Sanskar Shinde <136722408+sanskar1shinde@users.noreply.github.com>
2025-06-22 19:04:04 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+function validate()
+{
+
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+     
+
+        alert("Login Succesfully");
+         
+    
+      
+
+
+}
